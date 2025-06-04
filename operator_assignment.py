22e56383149bba445c6d2df5ff96a3f505dfa5a5
Aji-Use <baguskuncoroa123@gmail.com>
2025-06-04 08:16:11 -0700

    Belajar Opertor Assignment

---
+++ operator_assignment.py
@@ -0,0 +1,57 @@
+# Belajar Operator Assignment
+
+
+
+# Assignment +=
+print('======= Assignment += =========')
+a = 5
+print('Nilai a = ', a)
+
+a += 3
+print('a += 3')
+print('Nilai a sekarang = ', a)
+
+# Assignment -=
+print('\n======= Assignment -= =========')
+b = 5
+print('Nilai b = ', b)
+
+b -= 2
+print('b -= 2')
+print('Nilai b sekarang = ', b)
+
+# Assignment *=
+print('\n======= Assignment *= =========')
+c = 5
+print('Nilai b = ', b)
+
+c *= 2
+print('c *= 2')
+print('Nilai b sekarang = ', c)
+
+# Assignment /=
+print('\n======= Assignment /= =========')
+d = 5
+print('Nilai d = ', d)
+
+d /= 2
+print('d /= 2')
+print('Nilai d sekarang = ', d)
+
+# Assignment %=
+print('\n======= Assignment %= =========')
+e = 7
+print('Nilai e = ', e)
+
+e %= 2
+print('e %= 2')
+print('Nilai e sekarang = ', e)
+
+# Assignment //=
+print('\n======= Assignment //= =========')
+f = 7
+print('Nilai f = ', f)
+
+f //= 2
+print('f //= 2')
+print('Nilai f sekarang = ', f)
