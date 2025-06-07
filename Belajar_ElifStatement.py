f4d5d98c2abf65949d3cb8d65adf5b6e5d6d8eca
Aji-Use <baguskuncoroa123@gmail.com>
2025-06-07 04:20:42 -0700

    Belajar elif statement

---
+++ Belajar_ElifStatement.py
@@ -0,0 +1,9 @@
+# Belajar Elif Statement
+data = input("Masukkan Username : ")
+
+if data == "yanto":
+    print(f"Selamat datang {data} sebagai admin di program kami")
+elif data == "karto":
+    print(f"Selamat datang {data} sebagai user di program kami")
+else:
+    print("Silahkan masukkan username yang sesuai")
