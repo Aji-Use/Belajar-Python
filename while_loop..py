30baf9ab9d5f2e3e4aea03c2d69a41dd3b3e3b24
Aji-Use <baguskuncoroa123@gmail.com>
2025-06-07 19:22:55 -0700

    Belajar while loop

---
+++ while_loop..py
@@ -0,0 +1,17 @@
+# Belajar while loop
+
+# ===== Infinity Loop, karena kondisi "angka(10) akan selalu lebih besari dari 5"
+#angka = 10
+
+#while angka > 5: #kontrol dengan kndisi boolean True / false
+    #print("loop")
+    
+    
+# ==== while loop, kontrol dengan logika
+angka = 1
+
+while angka <= 10:
+    angka += 1
+    print(f"Ini adalah looping ke-{angka}")
+    
+print("Akhir dari program")
