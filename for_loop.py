d8eeddd970dc67f3225681bf77cfe7523eb87e94
Aji-Use <baguskuncoroa123@gmail.com>
2025-06-07 18:49:24 -0700

    Belajar For Loop

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+# Belajar for loop dengan list data
+data = ["luffy", "zoro", "sanji", "nami",  "usop", "franky", "brok"]
+
+for i in data:
+    print(f"Anggota Topi Jerami : {i}")
+    
+print("End Program")
+
+# Belajar for loop dengan range
+print("\n===Belajar for loop dengan range===")
+for i in range(10):
+    print(f"Data dalam range : {i}")
+    
+# Belajar for loop dengan rangedengan indexing
+print("\n===Belajar for loop dengan rangedengan indexing===")
+for i in range(1,10):
+    print(f"Data dalam range : {i}")
