e632f44226d901fa7d2257a29f3c2e41d49cd622
Aji-Use <baguskuncoroa123@gmail.com>
2025-06-09 20:35:42 -0700

    Belajar data Tuple dan Sets

---
+++ tuple_sets.py
@@ -0,0 +1,34 @@
+# Belajar tuple dan set
+
+
+
+# ========= Data Tuple ==========
+"""
+    1. data tuple tidak bisa dirubah/manipulasi
+    2. operasi list tidak bisa digunakan
+    3. datanya konstan, untuk pengiriman data agar tidak berubah
+"""
+print("\n========= Data Tuple ==========")
+
+data_tuple = (1,2,3,4,5)
+
+# contoh operasi list pada tuple (TIDAK BISA)
+# data_tuple[1] = "legowo"
+
+print(f"Data tuple : {data_tuple}")
+
+
+
+# ====== Data Set =======
+"""
+    Data yang tidak memiliki index,
+    Operasi list tidak bisa dilakukan
+"""
+print("\n========= Data Set ==========")
+
+data_set = {1,2,3,4}
+
+print(f"Data set : {data_set}")
+
+
+
